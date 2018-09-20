7d6ba1147353a5f12a7aa1cddf7e335b166600a5
ShraddhaGithub123 <43431089+ShraddhaGithub123@users.noreply.github.com>
2018-09-20 11:20:33 +0530

    Add files via upload

---
+++ Mycls1.java
@@ -0,0 +1,31 @@
+package mypack;
+
+public class Mycls1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][] arr=new int[][]{{21,22},{32,25},{25,58}};
+for (int i = 0; i < arr.length; i++) {
+	for (int j = 0; j < 2; j++) {
+		System.out.println(arr[i][j]+" ");
+		
+	}
+	System.out.println(" ");
+	
+}
+//int[][][] arr2=new int[][][] {{{10,12},{30}},{{30,15},{80}}};
+//for (int i = 0; i < arr2.length; i++) {
+//	for (int j = 0; j < 2; j++) {
+//		for (int j2 = 0; j2 < 3; j2++) {
+//			System.out.println(arr2[i][j][j2]+" ");
+//			
+//		}
+//		System.out.println(" ");
+//		
+//	}
+//	
+//}
+	}
+
+}
